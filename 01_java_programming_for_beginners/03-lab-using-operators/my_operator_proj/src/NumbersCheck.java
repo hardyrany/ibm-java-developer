235c6175f637e84bb10af84e5fe05338e2c0eb09
Hardy Fernandes <hardymtf@gmail.com>
2026-04-14 10:01:39 -0100

    Array with mathematical operator

---
+++ 01_java_programming_for_beginners/03-lab-using-operators/my_operator_proj/src/NumbersCheck.java
@@ -0,0 +1,18 @@
+public class NumbersCheck {
+    public static void main(String[] args) {
+
+        int[] intArray = { 12, 17, 3, 165, 87, 15, 54, 123, 98, 23 };
+        int additionArr = intArray[0] + intArray[1];
+        int subtractionArr = intArray[2] - intArray[3];
+        int multiplicationArr = intArray[4] * intArray[5];
+        int divisionArr = intArray[6] / intArray[7];
+        int modulusArr = intArray[8] % intArray[9];
+
+        System.out.println(additionArr);
+        System.out.println(subtractionArr);
+        System.out.println(multiplicationArr);
+        System.out.println(divisionArr);
+        System.out.println(modulusArr);
+
+    }
+}

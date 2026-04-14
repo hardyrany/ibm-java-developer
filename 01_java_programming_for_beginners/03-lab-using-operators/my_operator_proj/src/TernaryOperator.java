28fe317a54cdc20cec29427870790b0df73c43b8
Hardy Fernandes <hardymtf@gmail.com>
2026-04-14 09:36:04 -0100

    Ternary operator with args

--- 01_java_programming_for_beginners/03-lab-using-operators/my_operator_proj/src/TernaryOperator.java
+++ 01_java_programming_for_beginners/03-lab-using-operators/my_operator_proj/src/TernaryOperator.java
@@ -3,5 +3,8 @@ public class TernaryOperator {
 
         String hasArgs = args.length == 0 ? "No arguments passed" : args[0];
         System.out.println(hasArgs);
+
+        String withArgs = args.length == 0 ? "something" : args[0];
+        System.out.println(withArgs);
     }
 }

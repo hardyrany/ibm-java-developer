dc352288454af1fa23d844fb518aa1884264d627
Hardy Fernandes <hardymtf@gmail.com>
2026-04-21 14:40:35 -0100

    style: method identification

--- 01_java_programming_for_beginners/04-coding-cheat-sheet-building-blocks-of-java-programming/Car.java
+++ 01_java_programming_for_beginners/04-coding-cheat-sheet-building-blocks-of-java-programming/Car.java
@@ -3,6 +3,8 @@ public class Car {
     // Attributes
     String color;
     int year;
+
+    // Method without return value
     void displayInfo() {
         System.out.println("Color: " + color + "\nYear: " + year );
     }

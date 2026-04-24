ce35fcca3a016cf761de1fce425dc5e90a47a8e9
Hardy Fernandes <hardymtf@gmail.com>
2026-04-24 07:44:46 -0100

    feat: arithmetic operators

---
+++ 01_java_programming_for_beginners/04-coding-cheat-sheet-building-blocks-of-java-programming/ArithmeticOperators.java
@@ -0,0 +1,22 @@
+
+// The following operators perform basic mathematical functions
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+
+        /*
+         * Static variable
+         * Declared and initialized
+         */
+        int a = 10;
+        int b = 5;
+
+        System.out.println("First value: " + a);
+        System.out.println("Secund value: " + b);
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulus: " + (a % b));
+
+    }
+}

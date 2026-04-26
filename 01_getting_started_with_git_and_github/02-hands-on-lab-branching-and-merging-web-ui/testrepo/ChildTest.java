9c8520c53ac2df04c5d8991e9cb5dea77b947d8b
Hardy Fernandes <hardymtf@gmail.com>
2026-04-26 10:29:14 -0100

    feat: testrepo lab

---
+++ 01_getting_started_with_git_and_github/02-hands-on-lab-branching-and-merging-web-ui/testrepo/ChildTest.java
@@ -0,0 +1,8 @@
+// Adding a new file in the child branch
+
+public class ChildTest {
+  public static void main(String[] args) {
+
+    System.out.println("Inside Child Branch")
+  }
+}
---
+++ 01_getting_started_with_git_and_github/02-hands-on-lab-branching-and-merging-web-ui/testrepo/FirstJava.java
@@ -0,0 +1,8 @@
+// Display the output
+
+public class FirstJava {
+  public static void main(String[] args) {
+
+    System.out.println("First Java");
+  }
+}

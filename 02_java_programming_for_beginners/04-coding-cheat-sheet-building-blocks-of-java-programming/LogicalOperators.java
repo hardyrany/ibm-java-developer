1980c1a14d636651911217fa17481956f534e4dc
Hardy Fernandes <hardymtf@gmail.com>
2026-04-27 17:45:32 -0100

    feat: logical operators

---
+++ 02_java_programming_for_beginners/04-coding-cheat-sheet-building-blocks-of-java-programming/LogicalOperators.java
@@ -0,0 +1,21 @@
+
+/* 
+  You can use logical to combine boolean expressions
+  The following code examples use boolean x = true; and boolean y = false
+ */
+
+public class LogicalOperators {
+
+    public static void main(String[] args) {
+        
+        // Static variables
+        // Primitive data type
+        // Declared and initialized
+
+        int a = 10;
+        int b = 5;
+
+        
+
+    }
+}

01a39f828652fb6a789244a3025bc75901f086b8
Hardy Fernandes <hardymtf@gmail.com>
2026-04-27 17:48:19 -0100

    feat: class

---
+++ 02_java_programming_for_beginners/04-coding-cheat-sheet-building-blocks-of-java-programming/Car.java
@@ -0,0 +1,12 @@
+// Class
+public class Car {
+
+    // Attributes
+    String color;
+    int year;
+
+    // Method without return value
+    void displayInfo() {
+        System.out.println("Color: " + color + "\nYear: " + year );
+    }
+}

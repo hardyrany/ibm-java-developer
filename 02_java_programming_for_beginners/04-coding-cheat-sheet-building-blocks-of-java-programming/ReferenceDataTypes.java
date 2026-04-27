4ae8233e840a6e9a3ea99af65236a1836ba506e2
Hardy Fernandes <hardymtf@gmail.com>
2026-04-27 17:42:01 -0100

    feat: reference data types

---
+++ 02_java_programming_for_beginners/04-coding-cheat-sheet-building-blocks-of-java-programming/ReferenceDataTypes.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class ReferenceDataTypes {
+    public static void main(String[] args) {
+
+        // String variable
+        String greeting = "Hello, World!";
+
+        // Array integer variable
+        int[] scores = { 85, 90, 78, 92 };
+
+        System.out.println(greeting);
+        System.out.println(Arrays.toString(scores));
+
+    }
+}

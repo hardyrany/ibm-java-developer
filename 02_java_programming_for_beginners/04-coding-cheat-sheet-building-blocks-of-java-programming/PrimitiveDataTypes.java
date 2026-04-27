3333d120b487d905d1366f9e1cc3d0824d020af8
Hardy Fernandes <hardymtf@gmail.com>
2026-04-27 17:41:31 -0100

    feat: primitive data types

---
+++ 02_java_programming_for_beginners/04-coding-cheat-sheet-building-blocks-of-java-programming/PrimitiveDataTypes.java
@@ -0,0 +1,23 @@
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+
+        byte age = 25; // Age of a person
+        short temperature = 5; // Temperature in degree
+        int population = 1000000; // Population of city
+        long distanceToMoon = 384400000L; // Distance in meters
+        float price = 19.99f; // Price of a product
+        double pi = 3.141592653589793; // Value of Pi
+        char initial = 'A'; // Initial of a person's name
+        boolean isLoggedIn = true; // User login status
+
+        System.out.println("Age of a person: " + age);
+        System.out.println("Temperature in degree: " + temperature);
+        System.out.println("Population of city: " + population);
+        System.out.println("Distance in meters: " + distanceToMoon);
+        System.out.println("Price of a product: " + price);
+        System.out.println("Value od Pi: " + pi);
+        System.out.println("Initial of a person's name: " + initial);
+        System.out.println("User login status: " + isLoggedIn);
+
+    }
+}

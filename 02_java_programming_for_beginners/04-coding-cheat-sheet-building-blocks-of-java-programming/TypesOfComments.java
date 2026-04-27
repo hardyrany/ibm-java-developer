9db86675003fe0324431f11c7992909ca79a03e3
Hardy Fernandes <hardymtf@gmail.com>
2026-04-27 17:31:55 -0100

    feat: types of comments

---
+++ 02_java_programming_for_beginners/04-coding-cheat-sheet-building-blocks-of-java-programming/TypesOfComments.java
@@ -0,0 +1,35 @@
+public class TypesOfComments {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+        System.out.println("// This is a single-line comment.");
+
+        int number = 10; // This variable stores the number 10
+        System.out.println("// This variable stores the number " + number);
+        System.out.println(
+                "/* This is a multi-line comment It can be used to explain a block of code or provide detailed information */");
+        System.out.println("/* int sum = 0; This variable will hold the sum of numbers */");
+
+        System.out.println("""
+                /**
+                 * This method calculates the square of number.
+                 * 
+                 * @param number The number to be squared
+                 * @return The square of the input number
+                 */
+                """);
+
+    }
+
+    // Method with return value and parameter
+    /**
+     * This method calculates the square of number.
+     * 
+     * @param number The number to be squared
+     * @return The square of the input number
+     */
+    public int square(int number) {
+        return number * number;
+    }
+
+}

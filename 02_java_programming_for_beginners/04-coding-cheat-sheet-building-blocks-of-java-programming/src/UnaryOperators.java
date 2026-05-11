ac444e54c4f8a8ac527d2a58d02f70a4b8cce77f
Hardy Fernandes <hardymtf@gmail.com>
2026-05-11 14:38:44 -0100

    feat: unary operators

---
+++ 02_java_programming_for_beginners/04-coding-cheat-sheet-building-blocks-of-java-programming/src/UnaryOperators.java
@@ -0,0 +1,9 @@
+public class UnaryOperators {
+    
+    public static void main(String[] args) {
+
+        int a = 10;
+        int positive = +a;
+        System.out.println("Unary plus: " + (positive));
+    }
+}

4c49bdb6e1855fa6cb2a81c6ef989c35cacf9a72
Hardy Fernandes <hardymtf@gmail.com>
2026-05-11 14:47:41 -0100

    feat: ternary operator

---
+++ 02_java_programming_for_beginners/04-coding-cheat-sheet-building-blocks-of-java-programming/src/TernaryOperators.java
@@ -0,0 +1,12 @@
+public class TernaryOperators {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        // If a is greater than b, assign a, otherwise assign b
+        // variable = condition, ternary symbol (?) conditional variable ternary symbol (:) conditional variable
+        int max = (a > b) ? a : b;
+        System.out.println(" Maximum value is: " + max);
+    }
+}
